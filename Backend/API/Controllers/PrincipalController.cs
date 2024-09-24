f6ea4676227d4055ea0a614885946a1052ebdc9d
Jenil Vora <jenil.vora0408@gmail.com>
2024-09-24 14:00:13 +0530

    uncommented principal policy

--- Backend/API/Controllers/PrincipalController.cs
+++ Backend/API/Controllers/PrincipalController.cs
@@ -9,7 +9,7 @@ namespace API.Controllers;
 
 [ApiController]
 [Route("api/principal")]
-// [PrincipalPolicy]
+[PrincipalPolicy]
 public class PrincipalController(IPrincipalService principalService) : BaseController
 {
     #region Constructor

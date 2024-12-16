970615bf5537857022610c96a98664b878f86b4d
Jenil Vora <jenil.vora0408@gmail.com>
2024-12-16 18:15:22 +0530

    completed UI for subjects list in students' panel

---
+++ Frontend/SchoolManagementSystem/src/app/models/student/subjects-list.ts
@@ -0,0 +1,9 @@
+export interface ISubjectsListForStudentsInterface{
+    classId: number;
+    className: string;
+    subjectId: number;
+    subjectName: string;
+    subjectCode: string;
+    subjectTeacherId: number;
+    subjectTeacherName: string;
+}
